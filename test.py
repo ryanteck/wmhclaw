2d6b12c40ee359658b2f4eb065bd8dc61cfc1858
Ryan Walmsley <ryanteck@gmail.com>
2017-10-10 20:35:10 +0100

    Fixed readline to rawinput

--- test.py
+++ test.py
@@ -14,17 +14,17 @@ leftButton = Button(3)
 #downStop = Button()
 
 print("initialization test")
-readline()
+raw_input()
 
 print("Homing")
 
 leftRight.backward()
 forwardBack.forward()
-readline()
+raw_input()
 leftRight.stop()
 forwardBack.stop()
 print("Homed")
-readline()
+raw_input()
 print("Moving to the right")
 leftRight.forward()
 #Stop
