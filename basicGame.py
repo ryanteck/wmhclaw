14121e6ea67d5cf89aa168ddd84dcbf0a8e669df
Ryan Walmsley <ryan@ryanteck.uk>
2017-10-11 11:52:47 +0000

    fixed issues set 1

--- basicGame.py
+++ basicGame.py
@@ -1,8 +1,8 @@
 #Basic Claw Game
 from gpiozero import Motor, Button, LED
 from time import sleep
-from pygame import mixer
-
+import pygame
+pygame.mixer.init()
 #Init Motors
 forwardBack = Motor(17,27)
 leftRight = Motor(22,23)
@@ -27,29 +27,29 @@ clawMusic = "musicFiles/movement.mp3"
 while True:
     print("Press enter to start game")
     pygame.mixer.music.load(moveMusic)
-    raw_input()
+    input()
     print("Waiting for forward button")
     forwardButton.wait_for_press()
-    pygame.mixer.play()
+    pygame.mixer.music.play()
     forwardBack.forward()
     while(forwardButton.is_pressed) and (forwardBackStop.is_pressed != 1):
         pass
     forwardBack.stop()
-    pygame.mixer.pause()
+    pygame.mixer.music.pause()
     print("Waiting for right button")
     rightButton.wait_for_press()
-    pygame.mixer.unpause()
+    pygame.mixer.music.unpause()
     leftRight.forward()
     while(rightButton.is_pressed) and (leftRightStop.is_pressed != 1):
         pass
     leftRight.stop()
-    pygame.mixer.load(clawMusic)
-    pygame.mixer.play()
+    pygame.mixer.music.load(clawMusic)
+    pygame.mixer.music.play()
     #Claw Drop
     sleep(5) #Simulate claw drop
     #Claw Up
-    pygame.mixer.load(moveMusic)
-    pygame.mixer.play()
+    pygame.mixer.music.load(moveMusic)
+    pygame.mixer.music.play()
     #Home left
     leftRight.backward()
     sleep(0.5)
@@ -60,5 +60,6 @@ while True:
     sleep(0.5)
     forwardBackStop.wait_for_press()
     forwardBack.stop()
+    pygame.mixer.music.stop()
         
     
