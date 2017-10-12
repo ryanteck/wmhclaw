08adc2ae5cdb4719945e849918e0e5adbd1879f8
Ryan Walmsley <ryan@ryanteck.uk>
2017-10-12 13:18:18 +0000

    Fixed sound

--- musicFiles/musicTest.py
+++ musicFiles/musicTest.py
@@ -1,12 +1,12 @@
 import pygame
 import time
 start = time.time()
-pygame.mixer.pre_init(22050, -8, 2, 512)
+#pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.mixer.init()
-pygame.mixer.music.load("movement.ogg")
+pygame.mixer.music.load("oldMp3s/attract-2.mp3")
 pygame.mixer.music.play()
-time.sleep(5)
-pygame.mixer.music.stop()
+time.sleep(7)
+#pygame.mixer.music.stop()
 end = time.time()
 totalTime = end-start
 print(totalTime)
